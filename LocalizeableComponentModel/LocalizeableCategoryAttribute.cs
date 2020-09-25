7fd387643d432004cff082c2585c214aebbeb264
Brayconn <29030756-Brayconn@users.noreply.github.com>
2020-09-25 00:30:50 -0700

    Both classes now search through NonPublic properties as well as Public ones

--- LocalizeableComponentModel/LocalizeableCategoryAttribute.cs
+++ LocalizeableComponentModel/LocalizeableCategoryAttribute.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Reflection;
 
 namespace LocalizeableComponentModel
 {
     public sealed class LocalizeableCategoryAttribute : CategoryAttribute
     {
-        public LocalizeableCategoryAttribute(string key, Type resx) : base((string)resx.GetProperty(key).GetValue(resx))
+        public LocalizeableCategoryAttribute(string key, Type resx)
+            : base((string)resx.GetProperty(key, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public).GetValue(resx))
         {
 
         }
--- LocalizeableComponentModel/LocalizeableDescriptionAttribute.cs
+++ LocalizeableComponentModel/LocalizeableDescriptionAttribute.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Reflection;
 
 namespace LocalizeableComponentModel
 {
     public sealed class LocalizeableDescriptionAttribute : DescriptionAttribute
     {
-        public LocalizeableDescriptionAttribute(string key, Type resx) : base((string)resx.GetProperty(key).GetValue(resx))
+        public LocalizeableDescriptionAttribute(string key, Type resx)
+            : base((string)resx.GetProperty(key, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public).GetValue(resx))
         {
 
         }
